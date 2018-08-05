4d245787a97df746f263caf3791a06cfbfeda400
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-05 17:13:33 +0200

    Add Service Worker

---
+++ sw.js
@@ -0,0 +1,49 @@
+// Define Cache
+let staticCacheName = 'mws-restaurant-static-v1';
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+    return cache.addAll([
+      'index.html',
+      'restaurant.html',
+      '/css/main.css',
+      '/css/responsive.css',
+      '/js/dbhelper.js',
+      '/js/main.js',
+      '/js/restaurant_info.js',
+      '/img/*',
+      '/js/register-sw.js',
+      'https://fonts.googleapis.com/css?family=Open+Sans:300,400'
+    ])
+    .catch(error => {
+
+    });
+  }));
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+    .then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+})
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+        return (response || fetch(event.request);
+    })
+  );
+});
