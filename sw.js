368d791fe61c755c2e3d80f011a0fb5ece337d79
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-05 18:09:13 +0200

    Fix Service Worker registration

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
     .then(function(response) {
-        return (response || fetch(event.request);
+        return response || fetch(event.request);
     })
   );
 });
