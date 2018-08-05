06ba931da85e6ee0ed7f7eda30dc2bc6a093759e
zuckerwar <36587988+zuckerwar@users.noreply.github.com>
2018-08-05 23:25:47 +0200

    Fix Service Worker

--- sw.js
+++ sw.js
@@ -1,49 +1,47 @@
-// Define Cache
-let staticCacheName = 'mws-restaurant-static-v1';
+self.addEventListener('install', function(event) {
+  // Perform install steps
+}); var CACHE_NAME = 'restaurant-cache';
+var urlsToCache = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/register-sw.js',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  'https://fonts.googleapis.com/css?family=Open+Sans:300,400'
+];
 
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-    return cache.addAll([
-      'index.html',
-      'restaurant.html',
-      '/css/main.css',
-      '/css/responsive.css',
-      '/js/dbhelper.js',
-      '/js/main.js',
-      '/js/restaurant_info.js',
-      '/img/*',
-      '/js/register-sw.js',
-      'https://fonts.googleapis.com/css?family=Open+Sans:300,400'
-    ])
-    .catch(error => {
-
-    });
-  }));
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-    .then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('mws-restaurant-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-})
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-        return response || fetch(event.request);
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
     })
+    .catch(err => console.log(err, event.request))
   );
 });
